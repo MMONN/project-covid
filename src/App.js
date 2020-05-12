9904f1aae053e78aac293bf825f2c646e8eb1800
MMONN <chatchanok.cm@mail.kmutt.ac.th>
2020-05-12 12:58:00 +0700

    edit code App.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
+import './App.css'
 import logo from './logo.svg';
-import axios from 'axios'
-
+import axios from 'axios';
+import TotalCase from './TotalCase';
+import NewCase from './TotalCase';
 
 let contact = 'MMONN'
-class ProductList extends React.Component {
+class Covid extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      user: 'chatchanok',
-      product: [
-        {
-          productName: '',
-          productDetail: '',
-          price: ''
-        }
-      ],
-      persons: [],
       covid: {
         active_cases: '',
         deaths_per_1m_population: '',
@@ -31,91 +24,39 @@ class ProductList extends React.Component {
       }
     }
   }
-
-
-// componentDidMount() {
-//   axios.get('https://dry-scrubland-02499.herokuapp.com/api/v1/products')
-//     .then(res => {
-//       const newproducts = res.data;
-//       console.log(newproducts)
-//       this.setState({ product: newproducts })
-//     })
-// }
-// componentDidMount() {
-//   axios.get(`https://jsonplaceholder.typicode.com/users`)
-//     .then(res => {
-//       const persons = res.data;
-//       this.setState({ persons: persons });
-//       console.log(persons)
-//     })
-// }
-
-componentDidMount() {
-  const axios = require("axios");
-
-  axios({
-    "method": "GET",
-    "url": "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php",
-    "headers": {
-      "content-type": "application/octet-stream",
-      "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-      "x-rapidapi-key": "93cc938ca2mshd06030b874a1b28p12983bjsn26c8fec9fd67"
-    }
-  })
-    .then((response) => {
-      const inform = response.data;
-      this.setState({ covid: inform })
-      console.log(inform)
+  componentDidMount() {
+    const axios = require("axios");
+    axios({
+      "method": "GET",
+      "url": "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php",
+      "headers": {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "93cc938ca2mshd06030b874a1b28p12983bjsn26c8fec9fd67"
+      }
     })
-    .catch((error) => {
-      console.log(error)
-    })
+      .then((response) => {
+        const inform = response.data;
+        this.setState({ covid: inform })
+        console.log(inform)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    let total = { totalcase: this.state.covid.total_cases, newCase: this.state.covid.new_cases }
+    return (
+      <div className="container">
+        <div className="Head">
+          <h1>COVID 2019</h1>
+          <p>Statistic Coronavirus around the world</p>
+        </div>
+        <TotalCase mes={total} />
+      </div>
+    )
+  }
 }
 
-
-render() {
-  let users = 'MMONN'
-  return (
-    <div>
-      <div>
-        {this.state.product.map((product) => (
-          <ProductCard item={product} />
-        ))}
-        {/* <ul>
-            {this.state.persons.map(person => <li>{person.name}</li>)}
-          </ul> */}
-      </div>
-    </div>
-  )
-}
-}
-
-const ProductCard = (props) => {
-  return (
-    <div className="card">
-      <div className="image">
-        <img src={props.item.product_image} className="img-product"></img>
-      </div>
-
-      <div className="name">
-        <p className="product-name">
-          {props.item.product_name}
-        </p>
-      </div>
-
-      <div className="datail">
-        <p className="product-detail">
-          {props.item.product_detail}
-        </p>
-      </div>
-
-      <div className="price">
-        <p className="product-price">
-          {props.item.price} $
-        </p>
-      </div>
-    </div>
-  )
-}
-
-export default ProductList;
+export default Covid;
