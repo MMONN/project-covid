012c65f00c9d96955e77d6dcf3c2cec2b22c02e0
MMONN <chatchanok.cm@mail.kmutt.ac.th>
2020-07-06 14:06:22 +0700

    [Update] change code to react hook

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/App.css';
 import DataCovid from './component/DataCovid';
-// import ResultByCountry from './ResultByCountry';
 
-class Covid extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
+function Covid() {
+  const [covid, setCovid] = useState({
+    active_cases: '',
+    deaths_per_1m_population: '',
+    new_cases: '',
+    new_deaths: '',
+    serious_critical: '',
+    statistic_taken_at: '',
+    total_cases: '',
+    total_cases_per_1m_population: '',
+    total_deaths: '',
+    total_recovered: '',
+  })
 
-    }
-  }
-  componentDidMount() {
-    this.interval = setInterval(() => this.CovidInformation(), 1000 * 60 * 60 * 24)
-  }
+  useEffect(() => {
+    const axios = require("axios");
+    axios({
+      "method": "GET",
+      "url": "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php",
+      "headers": {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+        "x-rapidapi-key": "93cc938ca2mshd06030b874a1b28p12983bjsn26c8fec9fd67"
+      }
+    })
+      .then((response) => {
+        const inform = response.data;
+        setCovid(inform)
+        console.log(inform)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
 
+  }, [])
 
-
-  render() {
-    let date = Date(Date.now());
-    let newDate = date.toString();
-
-    return (
+  return (
+    <>
       <div className="container">
         <div className="Head">
           <h1>COVID 2019</h1>
           <p>Statistic Coronavirus around the world</p>
-          <p className="date">The current date is: {newDate} </p>
         </div>
-        <DataCovid />
-        {/* <ResultByCountry/> */}
+        <DataCovid data={covid} />
       </div>
-    )
-  }
+    </>
+  )
 }
-
 export default Covid;
--- src/component/DataCovid.js
+++ src/component/DataCovid.js
@@ -4,152 +4,82 @@ import '../css/DataCovidcss.css';
 import { TeamOutlined, PlusOutlined, GlobalOutlined, UserAddOutlined, HomeOutlined } from '@ant-design/icons';
 import styled, { css } from 'styled-components';
 
-class DataCovid extends React.Component {
-      constructor(props) {
-            super(props)
-            this.state = {
-                  covid: {
-                        active_cases: '',
-                        deaths_per_1m_population: '',
-                        new_cases: '',
-                        new_deaths: '',
-                        serious_critical: '',
-                        statistic_taken_at: '',
-                        total_cases: '',
-                        total_cases_per_1m_population: '',
-                        total_deaths: '',
-                        total_recovered: '',
-                  }
-            }
-      }
-      CovidInformation() {
-            const axios = require("axios");
-            axios({
-                  "method": "GET",
-                  "url": "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php",
-                  "headers": {
-                        "content-type": "application/octet-stream",
-                        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-                        "x-rapidapi-key": "93cc938ca2mshd06030b874a1b28p12983bjsn26c8fec9fd67"
-                  }
-            })
-                  .then((response) => {
-                        const inform = response.data;
-                        this.setState({ covid: inform })
-                        console.log(inform)
-                  })
-                  .catch((error) => {
-                        console.log(error)
-                  })
+function DataCovid(props) {
+      const Wrapper = styled.div`
+      width: 100%;
+      height: 25%;
+      background-color: #2874A6;
+      border-radius: 8px ;
+      text-align: center;
+      color: #fff;
+      font-size: 1.5rem;
+      font-weight: 500;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);  
+      display: flex;  
+      justify-content: center;
+      align-items: center;
+      margin: 0;
+      text-align: center;
+      font-family: 'Kanit', sans-serif;
 
-      }
-      componentDidMount() {
-            this.CovidInformation();
-      }
+      ${props => props.Active && css`
+      background: #58D68D;
+      `}
+      ${props => props.Recover && css`
+      background: #F4D03F;
+      `}
+      ${props => props.Deaths && css`
+      background: #EC7063
+    `}`;
 
-      render() {
 
-            const title = { total: 'TOTLE CASE', active: 'ACTIVE CASE', recover: 'TOTLE RECOVER', datehs: 'TOTLE DEATHS' }
-            const total = {
-                  totalcase: this.state.covid.total_cases, activecase: this.state.covid.active_cases, recovercase: this.state.covid.total_recovered
-                  , totaldeaths: this.state.covid.total_deaths
-            }
-            const update = { newCase: this.state.covid.new_cases, newdeaths: this.state.covid.new_deaths }
-
-            const Wrapper = styled.div`
-                  width: 100%;
-                  height: 25%;
-                  background-color: #2874A6;
-                  border-radius: 8px ;
-                  text-align: center;
-                  color: #fff;
-                  font-size: 1.5rem;
-                  font-weight: 500;
-                  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);  
-                  display: flex;  
-                  justify-content: center;
-                  align-items: center;
-                  margin: 0;
-                  text-align: center;
-                  font-family: 'Kanit', sans-serif;
-
-                  ${props => props.Active && css`
-                  background: #58D68D;
-                  `}
-                  ${props => props.Recover && css`
-                  background: #F4D03F;
-                  `}
-                  ${props => props.Deaths && css`
-                  background: #EC7063
-                `}`;
-                
-
-            return (
-                  <div className="box-container">
-                        <div className="first-box">
-                              <div className="box">
-                                    <Wrapper>
-                                          <i class="fas fa-hospital-user"></i>
-                                          <Title mes={title.total} />
-                                    </Wrapper>
-                                    <Result result={total.totalcase} />
-                                    <PlusOutlined />
-                                    <Update update={update.newCase} />
+      return (
+            <div className="box-container">
+                  <div className="first-box">
+                        <div className="box">
+                              <Wrapper>
+                                    <p>TOTOL CASE</p>
+                              </Wrapper>
+                              <div className="result">
+                                    {props.data.total_cases}
                               </div>
-                        </div>
-
-                        <div className="second-box">
-                              <div className="box">
-                                    <Wrapper Active>
-                                          <Title mes={title.active} />
-                                    </Wrapper>
-                                    <Result result={total.activecase} />
-                              </div>
-                              <div className="box">
-                                    <Wrapper Recover>
-                                          <Title mes={title.recover} />
-                                    </Wrapper>
-                                    <Result result={total.recovercase} />
-                              </div>
-                              <div className="box">
-                                    <Wrapper Deaths>
-                                          <Title mes={title.datehs} />
-                                    </Wrapper>
-                                    <Result result={total.totaldeaths} />
-                                    <PlusOutlined />
-                                    <Update update={update.newdeaths} />
+                              <PlusOutlined />
+                              <div className="update">
+                                    {props.data.new_cases}
                               </div>
                         </div>
                   </div>
-            )
-      }
-}
-
-const Title = (props) => {
-      return (
-            <div>
-                  <div className="Title">
-                        {props.mes}
+                  <div className="second-box">
+                        <div className="box">
+                              <Wrapper Active>
+                                    <p>ACTIVE CASE</p>
+                              </Wrapper>
+                              <div className="result">
+                                    {props.data.active_cases}
+                              </div>
+                        </div>
+                        <div className="box">
+                              <Wrapper Recover>
+                                    <p>TOTAL RECOVER</p>
+                              </Wrapper>
+                              <div className="result">
+                                    {props.data.total_recovered}
+                              </div>
+                        </div>
+                        <div className="box">
+                              <Wrapper Deaths>
+                                    <p>TOTAL DEATHS</p>
+                              </Wrapper>
+                              <div className="result">
+                                    {props.data.total_deaths}
+                              </div>
+                              <PlusOutlined />
+                              <div className="update">
+                                    {props.data.new_deaths}
+                              </div>
+                        </div>
                   </div>
             </div>
-
       )
 }
-
-const Result = (props) => {
-      return (
-            <div className="result">
-                  {props.result}
-            </div>
-      )
-}
-
-const Update = (props) => {
-      return (
-            <div className="update">
-                  {props.update}
-            </div>
-      )
-}
-
 export default DataCovid;
